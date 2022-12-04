473ec176cd7f9806b9e208ded0bae2feb134c77a
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-04 16:48:00 +0300

    global axios instance

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Title from './components/Title';
 import Setup from './examples/1-first-request';
 import HeaderSetup from './examples/2-headers';
 import PostRequest from './examples/3-post-request';
+import GlobalInstance from './examples/4-global-instance';
+import './axios/global';
 function App() {
   return (
     <main>
@@ -9,6 +11,7 @@ function App() {
       <Setup/>
       <HeaderSetup/>
       <PostRequest/>
+      <GlobalInstance/>
     </main>
   );
 }
