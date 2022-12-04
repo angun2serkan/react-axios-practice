de11db8cb63dc1a1b519065d0632756a85e0bb4c
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-12-04 17:22:58 +0300

    custom-axios-instance

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Setup from './examples/1-first-request';
 import HeaderSetup from './examples/2-headers';
 import PostRequest from './examples/3-post-request';
 import GlobalInstance from './examples/4-global-instance';
+import CustomInstance from './examples/5-custom-instance';
+
+
 import './axios/global';
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <HeaderSetup/>
       <PostRequest/>
       <GlobalInstance/>
+      <CustomInstance/>
     </main>
   );
 }
